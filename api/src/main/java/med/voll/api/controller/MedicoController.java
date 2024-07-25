85c8f71170803a319e0514f5e0ba996d862ef3ab
Diogo <diogo322008@hotmail.com>
2024-07-25 00:10:54 -0300

    metodo excluir medico, @DeleteMapping passando o id como variavel de caminho @PathVariable

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -35,6 +35,12 @@ public class MedicoController {
         var medico = repository.getReferenceById(dados.id());
         medico.atualizarInformacoes(dados);
     }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void excluir(@PathVariable Long id){
+        repository.deleteById(id);
+    }
 }
 
 
