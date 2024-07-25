6433a92042564039855ffc2cfb142a8379114408
Diogo <diogo322008@hotmail.com>
2024-07-24 23:51:11 -0300

    metodo atualiza medico, @PutMapping

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -88,4 +88,16 @@ public class Medico {
     public Long getId() {
         return id;
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoMedico dados) {
+        if(dados.nome() != null){
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null){
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null){
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
 }
